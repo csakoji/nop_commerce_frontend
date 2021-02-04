f516b5e8c646853b384be19ad7e1911ecfa29226
csakoji <chaitanyasakoji0@gmail.com>
2021-02-04 19:50:48 +0530

    Updated conftest and test_login for loggin, added custom_logging.py

--- test_cases/conftest.py
+++ test_cases/conftest.py
@@ -1,14 +1,20 @@
 import pytest
 from selenium import webdriver
+from utilities.custom_logging import LogGen
+
+logger = LogGen.log_gen()
 
 
 @pytest.fixture(scope='function')
 def setup(browser):
     if browser == 'firefox':
+        logger.info("Opening firefox browser")
         driver = webdriver.Firefox()
     else:
+        logger.info("Opening chrome browser")
         driver = webdriver.Chrome()
     yield driver
+    logger.info("Quitting browser")
     driver.quit()
 
 
--- test_cases/test_login.py
+++ test_cases/test_login.py
@@ -1,11 +1,17 @@
 from utilities.readconfig import ReadConfig
+from utilities.custom_logging import LogGen
 
 
 class TestCasesForLogin:
     home_page = ReadConfig.get_home_page()
+    logger = LogGen.log_gen()
 
     def test_login(self, setup):
+        self.logger.info("Executing 'test_login'")
         self.driver = setup
+        self.logger.info("Opening home page of website")
         self.driver.get(self.home_page)
+        self.logger.info("Extraced title of the webpage")
         login_title = self.driver.title
-        assert "nopCommerce demo store" == login_title
+        assert "nopCommerce demo store" == login_title, 'Verifying webpage title'
+        self.driver.save_screenshot(".\\Screenshots\\" + 'test_homepageTitle.png')
